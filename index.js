c7501347d3feb8c5f70b4637f1781d1184ee29a8
Tyler Grayson <t.grayson311@gmail.com>
2020-06-20 01:23:59 -0500

    Ascii to Base64 encyption to share secret message

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+// destructure object to utilize hash property
+// decode back into ascii string
+const { hash } = window.location;
+const message = atob(hash.replace('#', ''));
+
+// check if a hash exists in URL and display encypted message
+if (message) {
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#message-show').classList.remove('hide');
+
+	document.querySelector('h1').innerHTML = message;
+}
+
 // stop default browser behavior to submit form to backend server
 document.querySelector('form').addEventListener('submit', (event) => {
 	event.preventDefault();
 
-	const input = document.querySelector('input');
-	console.log(input.value);
+	// hide input and display link when user submits
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#link-form').classList.remove('hide');
+
+	// get user input and encrypt the message
+	const input = document.querySelector('#message-input');
+	const encrypted = btoa(input.value);
+
+	// insert encypted message into link input for user to copy
+	const linkInput = document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
 });
